057d95538298fcd68ce773a016a652b0c17da654
TheBigCheesish <bigishcheese@gmail.com>
2026-03-18 23:24:54 +0000

    Refixed cropinfo

--- src/main/java/com/Nxer/TwistSpaceTechnology/common/ic2Crop/CropInfo.java
+++ src/main/java/com/Nxer/TwistSpaceTechnology/common/ic2Crop/CropInfo.java
@@ -8,10 +8,14 @@ import net.minecraft.item.ItemStack;
 import com.Nxer.TwistSpaceTechnology.common.GTCMItemList;
 import com.Nxer.TwistSpaceTechnology.common.ic2Crop.crops.PurpleMagnoliaCrop;
 
-import ic2.api.crops.CropCard;
-import ic2.api.crops.Crops;
 
-public class CropInfo {
+//import ic2.api.crops.CropCard;
+//import ic2.api.crops.Crops;
+
+import com.gtnewhorizon.cropsnh.api.CropCard;
+import com.gtnewhorizon.cropsnh.api.CropsNHCrops;
+
+/*public class CropInfo {
 
     private static final List<CropInfo> CROPS = new ArrayList<>();
 
@@ -37,9 +41,17 @@ public class CropInfo {
 
     public static void registerAllCropInfo() {
         for (CropInfo cropInfo : CROPS) {
+
             Crops.instance.registerCrop(cropInfo.getCropCard());
             Crops.instance.registerBaseSeed(cropInfo.getBaseSeed(), cropInfo.getCropCard(), 1, 1, 1, 1);
+
+            CropsNHCrops.instance.registerCrop(cropInfo.getCropCard());
+            CropsNHCrops.instance.registerBaseSeed(cropInfo.getBaseSeed(), cropInfo.getCropCard(), 1, 1, 1, 1);
+
         }
     }
 
 }
+
+*/
+
